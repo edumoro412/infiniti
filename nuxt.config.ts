73a40f6c560a1ea3eb5d6dd9fa4e109d5b5be75c
edumoro412 <edumoro2003@gmail.com>
2025-04-21 13:26:03 +0200

    update nuxt configuration and package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  runtimeConfig: {
+    currentsApiKey: process.env.NUXT_PUBLIC_APITUBE_API_KEY,
+    public: {},
+  },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/content',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+});
