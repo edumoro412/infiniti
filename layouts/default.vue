40e2cd0d882a12576ab8d57638e305987c940417
edumoro412 <edumoro2003@gmail.com>
2025-04-29 13:49:13 +0200

    remove commented-out icon for latest news in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
   <div>
     <!--     <Icon name="material-symbols:sports-football-rounded" />  Este es el icono de la app
     <Icon name="solar:infinity-bold" style="font-size: 8em" /> Este es el de deportes -->
-    <!--     <Icon name="ic:round-watch-later" /> Este es el de las ultimas noticias -->
+
     <HeaderComponent />
     <slot />
   </div>
