6e50111338c420d8288fb4e677e2a127d0f29dbc
edumoro412 <edumoro2003@gmail.com>
2025-04-29 12:17:49 +0200

    add HeaderComponent with template and styles

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="header">
+    <div class="header__logo">
+      <Icon name="solar:infinity-bold" style="font-size: 3.3em" />
+      <h1>INFINITI</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  background-color: var(--c-primary);
+  color: var(--c-secondary);
+  padding: 0.5em;
+  display: flex;
+
+  &__logo {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+  }
+}
+</style>
