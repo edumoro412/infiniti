5f7690b9c611d31f51347356711e5d8b4c445eda
edumoro412 <edumoro2003@gmail.com>
2025-04-29 12:17:38 +0200

    add default layout component with header and slot

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <!--     <Icon name="material-symbols:sports-football-rounded" />  Este es el icono de la app
+    <Icon name="solar:infinity-bold" style="font-size: 8em" /> Este es el de deportes -->
+    <!--     <Icon name="ic:round-watch-later" /> Este es el de las ultimas noticias -->
+    <HeaderComponent />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
