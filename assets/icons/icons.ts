7d6b25ff104a802655767835c57dd75fde87135a
edumoro412 <edumoro2003@gmail.com>
2025-05-05 13:06:13 +0200

    feat: add loader SVG icon for enhanced loading indication

---
+++ assets/icons/icons.ts
@@ -0,0 +1,33 @@
+export const icons = {
+  loader: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+        width="205"
+        height="205"
+        style="shape-rendering: auto; display: block; background: #fff;"
+      >
+        <g>
+          <path
+            style="transform:scale(0.8);transform-origin:50px 50px"
+            stroke-linecap="round"
+            d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40
+               C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z"
+            stroke-dasharray="42.76 42.76"
+            stroke-width="8"
+            stroke="#000"
+            fill="none"
+          >
+            <animate
+              attributeName="stroke-dashoffset"
+              values="0;256.58892822265625"
+              keyTimes="0;1"
+              dur="1.176s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </svg>
+    `,
+};
