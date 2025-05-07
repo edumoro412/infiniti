ed45c84ecc51fb802538fe2b610e7fc418a458d6
edumoro412 <edumoro2003@gmail.com>
2025-05-07 21:53:31 +0200

    refactor: simplify news fetching logic and ensure unique articles are returned

--- server/api/news.ts
+++ server/api/news.ts
@@ -1,35 +1,30 @@
-import type { NewsArticle, NewsResponse } from "~/interfaces/api/new";
+import type { NewsResponse } from "~/interfaces/api/new";
+
 export default defineEventHandler(async () => {
   const config = useRuntimeConfig();
-  let url = `https://newsdata.io/api/1/latest?apikey=${config.currentsApiKey}&language=es&country=es&category=top`;
-  let allNews: NewsArticle[] = [];
-  let pageCounter = 0;
+  const url = `https://newsdata.io/api/1/latest?apikey=${config.currentsApiKey}&language=es&country=es&category=top`;
 
   try {
-    while (url && pageCounter < 3) {
-      const response: NewsResponse = await $fetch(url, {
-        method: "GET",
-      });
+    const response: NewsResponse = await $fetch(url, {
+      method: "GET",
+    });
 
-      if (response.status === "success") {
-        allNews = allNews.concat(response.results);
-        url = response.nextPage
-          ? `https://newsdata.io/api/1/latest?apikey=${config.currentsApiKey}&language=es&category=top&page=${response.nextPage}`
-          : "";
-        pageCounter++;
-      } else {
-        throw createError({
-          statusCode: 500,
-          statusMessage: "Error interno al obtener noticias",
-        });
-        break;
-      }
+    if (response.status !== "success" || !response.results) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Error interno al obtener noticias",
+      });
     }
-    console.log("Estas son todas las noticias", allNews);
+
+    const uniqueNews = response.results.filter(
+      (article, index, self) =>
+        index === self.findIndex((a) => a.title === article.title)
+    );
+
     return {
       status: "success",
-      totalResults: allNews.length,
-      results: allNews,
+      totalResults: uniqueNews.length,
+      results: uniqueNews,
     };
   } catch (error: unknown) {
     console.error("Error al obtener noticias:", error);
