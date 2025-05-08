a3d925983a1dbe1eaeb0ea889164ee76951c8db5
edumoro412 <edumoro2003@gmail.com>
2025-05-08 12:00:25 +0200

    refactor: streamline theme handling and improve loading state display

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import type { NewsArticle, NewsResponse } from "~/interfaces/api/new";
+import { Icon } from "#components";
 import { useThemeStore } from "~/stores/theme";
 
+const store = useThemeStore();
+const darkTheme = computed(() => store.darkTheme);
 const loading = ref(false);
 const articles = ref<NewsArticle[]>([]);
-const themeStore = useThemeStore();
-const darkTheme = computed(() => themeStore.darkTheme);
 
 const fetchData = async () => {
   const cachedData = localStorage.getItem("newsData");
@@ -42,40 +43,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container" :class="{ 'dark-theme': darkTheme }">
-    <h1>Noticias</h1>
-    <div v-if="loading" class="loader__container">
-      <Icon name="line-md:loading-twotone-loop" />
+  <div>
+    <div
+      v-if="loading"
+      class="loader__container"
+      :class="{ 'dark-theme': darkTheme }"
+    >
+      <Icon name="line-md:loading-twotone-loop" class="dark-theme__icon" />
     </div>
-    <ul v-else>
-      <li v-for="article in articles" :key="article.title">
-        <h2>{{ article.title }}</h2>
-        <p>{{ article.description }}</p>
-        <a :href="article.link" target="_blank">Leer más</a>
-        <img
-          :src="article.image_url || '/noticia.png'"
-          :alt="`Imagen de la noticia: ${article.title}`"
-        />
-      </li>
-    </ul>
+    <div v-else>
+      <NewsDisplay :articles="articles" />
+    </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.container {
-  min-height: 100vh;
-}
+<style lang="scss" scoped>
 .loader__container {
+  background-color: var(--c-fourth);
+  min-height: 100vh;
+  padding: 10em 0;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 2em;
 }
 .dark-theme {
-  background-color: var(--c-fourth);
-  color: var(--c-secondary);
-}
-.dark-theme-icon {
-  filter: invert(1);
+  background-color: var(--c-secondary);
+  &__icon {
+    filter: invert(1);
+  }
 }
 </style>
