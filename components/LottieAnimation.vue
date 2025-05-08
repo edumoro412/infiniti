a176881b55fd45e51c15f1ae152a4e2699c7c484
edumoro412 <edumoro2003@gmail.com>
2025-05-08 13:30:14 +0200

    feat: add LottieAnimation component for animated graphics

---
+++ components/LottieAnimation.vue
@@ -0,0 +1,26 @@
+<script setup>
+onMounted(() => {
+  if (!customElements.get("dotlottie-player")) {
+    const script = document.createElement("script");
+    script.src =
+      "https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs";
+    script.type = "module";
+    document.body.appendChild(script);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <dotlottie-player
+      src="https://lottie.host/9a4773b0-ca75-4878-91a8-f39ae05bccca/QE0GdvoeeH.lottie"
+      background="transparent"
+      speed="1"
+      style="width: 300px; height: 300px"
+      loop
+      autoplay
+    />
+  </div>
+</template>
+
+<style scoped></style>
