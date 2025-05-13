2f281a546744f0b4c615b98d4d82eb28cef473fb
edumoro412 <edumoro2003@gmail.com>
2025-05-13 10:16:58 +0200

    feat: implement search functionality to fetch news articles with pagination

---
+++ server/api/search.ts
@@ -0,0 +1,58 @@
+import type { NewsArticle, NewsResponse } from "~/interfaces/api/new";
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const searchItem = query.q;
+
+  const config = useRuntimeConfig();
+  let url = `https://newsdata.io/api/1/latest?apikey=${config.currentsApiKey}&language=es&country=es&qInTitle=${searchItem}`;
+  let allNews: NewsArticle[] = [];
+  const seenTitles = new Set<string>();
+  let pageCounter = 0;
+
+  try {
+    while (url && pageCounter < 3) {
+      const response: NewsResponse = await $fetch(url, {
+        method: "GET",
+      });
+
+      if (response.status === "success") {
+        const uniqueResults = response.results.filter((article) => {
+          if (!article.title || seenTitles.has(article.title)) {
+            return false;
+          }
+          seenTitles.add(article.title);
+          return true;
+        });
+
+        allNews = allNews.concat(uniqueResults);
+
+        url = response.nextPage
+          ? `https://newsdata.io/api/1/latest?apikey=${config.currentsApiKey}&language=es&qInTitle=${searchItem}&page=${response.nextPage}`
+          : "";
+
+        pageCounter++;
+      } else {
+        throw createError({
+          statusCode: 500,
+          statusMessage: "Error interno al obtener noticias",
+        });
+      }
+    }
+
+    console.log("Estas son todas las noticias", allNews);
+    return {
+      status: "success",
+      totalResults: allNews.length,
+      results: allNews,
+    };
+  } catch (error: unknown) {
+    console.error("Error al obtener noticias:", error);
+    const message =
+      error instanceof Error ? error.message : "Error al obtener noticias";
+    throw createError({
+      statusCode: 500,
+      statusMessage: message,
+    });
+  }
+});
